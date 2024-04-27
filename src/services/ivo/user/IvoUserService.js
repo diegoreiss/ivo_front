47ff44b6ccf4150c1ac0518dc620e1c876fa7067
diegoreiss <diegoportal50@gmail.com>
2024-04-27 19:30:24 -0300

    reescrevendo método
    O método getAllAlunos foi movido para uma camada de service

---
+++ src/services/ivo/user/IvoUserService.js
@@ -0,0 +1,22 @@
+import cookieUtils from "@/utils/cookieUtils";
+
+
+export default class IvoUserService {
+  static endpoint = '/user';
+
+  constructor() {
+    this.cookie = cookieUtils.getCookie('ivo_access_token');
+  }
+
+  async getAllAlunos(path = `/role/aluno/`, method = 'GET') {
+    const request = await fetch(`${process.env.VUE_APP_IVO_API_URL}${IvoUserService.endpoint}${path}`, {
+      method,
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${this.cookie}`,
+      }
+    });
+
+    return await request.json();
+  }
+}
