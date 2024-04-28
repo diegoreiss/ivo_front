4530a079df9d03a7797aa62cbdd841865642fba5
diegoreiss <diegoportal50@gmail.com>
2024-04-28 19:10:15 -0300

    reformando arquivo e adicionando mais guars

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import cookieUtils from "@/utils/cookieUtils";
 import IvoUserService from "@/services/ivo/user/IvoUserService";
+import LoginView from "../views/LoginView.vue";
+import MudarSenhaView from "@/views/MudarSenhaView.vue";
+import HomePageView from "@/views/HomePageView.vue";
+import AlunosView from "@/views/AlunosView.vue";
+import PendenciasView from "@/views/PendenciasView.vue";
+import ChatView from "../views/ChatView.vue";
+import RelatoriosView from "@/views/RelatoriosView.vue";
 
 const routes = [
   {
@@ -9,50 +16,81 @@ const routes = [
     redirect: '/auth/login',
   },
   {
-    path: '/home',
-    name: 'HomePage',
-    meta: {
-      requiresAuth: true
-    },
-    component: () => import(/* webpackChunkName: "homepage" */ '../views/HomePageView.vue'),
-    children: [{
-      path: 'alunos',
-      name: 'home.alunos',
-      meta: {
-        requiredRole: 'admin',
+    path: '/auth',
+    name: 'login',
+    children: [
+      {
+        path: 'login',
+        name: 'auth.login',
+        component: LoginView,
+        meta: {
+          requireAuth: false,
+        }
       },
-      component: () => import(/* webpackChunkName: "alunos" */ '../views/AlunosView.vue'),
-    }, {
-      path: 'pendencias',
-      name: 'home.pendencias',
-      component: () => import(/* webpackChunkName: "pendencias" */ '../views/PendenciasView.vue'),
-    }, {
-      path: 'chat',
-      name: 'home.chat',
-      component: () => import(/* webpackChunkName: "conversas" */ '../views/ChatView.vue'),
-    }, {
-      path: 'relatorios',
-      name: 'home.relatorios',
-      component: () => import(/* webpackChunkName: "relatorios" */ '../views/RelatoriosView.vue'),
-    }],
+      {
+        path: 'mudarsenha',
+        name: 'auth.mudarsenha',
+        props: true,
+        beforeEnter: async (to) => {
+          const isAuth = await isAuthenticated();
+          if (!isAuth) {
+            return { name: 'auth.login' };
+          }
+
+          const ivoUserService = new IvoUserService(),
+            response = await ivoUserService.getCurrentUser();
+          
+          if (response.json.role === 2 && !response.json.is_password_changed) {
+            to.params.userId = response.json.pk;
+            return true;
+          } else {
+            return { name: 'HomePage' }
+          }
+        },
+        component: MudarSenhaView,
+        meta: {
+          requireAuth: true,
+        },
+      }
+    ]
   },
   {
-    path: '/auth/login',
-    name: 'LoginView',
-    component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue'),
+    path: '/home',
+    name: 'HomePage',
+    component: HomePageView,
+    meta: {
+      requireAuth: true,
+    },
+    children: [
+      {
+        path: 'alunos',
+        name: 'home.alunos',
+        component: AlunosView,
+      },
+      {
+        path: 'pendencias',
+        name: 'home.pendencias',
+        component: PendenciasView,
+      },
+      {
+        path: 'chat',
+        name: 'home.chat',
+        component: ChatView,
+      },
+      {
+        path: 'relatorios',
+        name: 'home.relatorios',
+        component: RelatoriosView,
+      }
+    ],
   },
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
-
-router.beforeEach(async (to) => {
-  if (to.path === '/auth/login') return;
-
+async function isAuthenticated() {
   const ivoUserService = new IvoUserService(),
     response = await ivoUserService.getCurrentUser();
+  
+  console.log(response.json);
 
   switch (response.status_code) {
     case 401:
@@ -60,12 +98,29 @@ router.beforeEach(async (to) => {
       cookieUtils.deleteCookie('ivo_access_token');
       cookieUtils.deleteCookie('ivo_refresh_token');
 
-      return { name: 'LoginView' };
+      return false;
+    case 200:
+      return true;
     default:
-      break;
+      return false;
   }
+}
 
-  console.log(response.json);
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+router.beforeEach(async (to) => {
+  if (!to.meta.requireAuth) {
+    return;
+  }
+  if (!isAuthenticated() && to.name !== 'auth.login') {
+    return { name: 'auth.login' };
+  }
+  if (to.meta.requireAuth && to.name === 'auth.mudarsenha') {
+    return;
+  }
 });
 
 export default router;
