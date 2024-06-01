b52eebe85d2b49439b61334a7ea040786807f4d4
Diego Reis <diegoportal.reis@outlook.com>
2024-06-01 19:57:58 -0300

    relatorios

--- src/views/RelatoriosView.vue
+++ src/views/RelatoriosView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="m-5">
     <h1>Relatórios</h1>
+    <iframe
+      src="http://localhost:3000/d-solo/cdnhue8wjha0wb/ivo-dashboard?orgId=1&refresh=1m&from=1717258780850&to=1717280380850&theme=light&panelId=1"
+      width="1000" height="300" frameborder="0"></iframe>
   </div>
 </template>
 
